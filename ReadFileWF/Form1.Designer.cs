fd97e14f3a3b552fd6ec2e3c884787473551c19f
pntuanhai-work <pntuanhai.work@gmail.com>
2025-10-28 22:51:18 +0700

    Upload winform app to read file .csv

---
+++ ReadFileWF/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace ReadFileWF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.lblKetQua = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 126);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(685, 248);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(83, 21);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(110, 59);
+            this.btnOpen.TabIndex = 1;
+            this.btnOpen.Text = "Mo file";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // lblKetQua
+            // 
+            this.lblKetQua.AutoSize = true;
+            this.lblKetQua.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblKetQua.ForeColor = System.Drawing.Color.Red;
+            this.lblKetQua.Location = new System.Drawing.Point(41, 94);
+            this.lblKetQua.Name = "lblKetQua";
+            this.lblKetQua.Size = new System.Drawing.Size(0, 20);
+            this.lblKetQua.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(685, 374);
+            this.Controls.Add(this.lblKetQua);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Label lblKetQua;
+    }
+}
+
---
+++ ReadFileWF/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ReadFileWF
+{
+
+    public partial class Form1 : Form
+    {
+        static DataTable ReadCsvFile(string filePath)
+        {
+            DataTable dataTB = new DataTable();
+
+            try
+            {
+                string[] lines = File.ReadAllLines(filePath, Encoding.UTF8);
+
+
+                foreach (string line in lines)
+                {
+                    string[] values = line.Split(',');
+
+                }
+                string[] headers = lines[0].Split(',');
+                foreach (string header in headers)
+                {
+                    dataTB.Columns.Add(header.Trim());
+                }
+
+                for (int i = 1; i < lines.Length; i++)
+                {
+                    string[] data = lines[i].Split(',');
+
+                    ///them neu cot khop
+                    if (data.Length == dataTB.Columns.Count)
+                    {
+                        // Tạo một DataRow mới và gán giá trị
+                        dataTB.Rows.Add(data.Select(d => (object)d.Trim()).ToArray());
+                    }
+                }
+                    Console.WriteLine("CSV file read successfully!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+
+
+            return dataTB;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                
+                if(ofd.ShowDialog() == DialogResult.OK)
+                {
+                    DataTable csvData = ReadCsvFile(ofd.FileName);
+                    dataGridView1.DataSource = csvData;
+                }
+            }
+            lblKetQua.Text = "Xử lý thành công ^^";
+        }
+    }
+}
